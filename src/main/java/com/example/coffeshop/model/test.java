43debe3df5b0f07056e05127520170a308836822
ias <iaszaen@gmail.com>
2022-07-14 13:56:55 +0700

    test update

--- src/main/java/com/example/coffeshop/model/test.java
+++ src/main/java/com/example/coffeshop/model/test.java
@@ -1,4 +1,9 @@
 package com.example.coffeshop.model;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 public class test {
+    public void test (){
+        System.out.println();
+    }
 }
