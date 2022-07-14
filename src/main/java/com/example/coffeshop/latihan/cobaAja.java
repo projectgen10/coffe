dc06f2c795d74de387f57dbee2f5b12701c93114
Arviena <arviena1004@gmail.com>
2022-07-14 12:16:31 +0700

    cobain

---
+++ src/main/java/com/example/coffeshop/latihan/cobaAja.java
@@ -0,0 +1,15 @@
+package com.example.coffeshop.latihan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class cobaAja {
+
+    @GetMapping("/hai")
+    public String hello(){
+        return "hai";
+    }
+}
