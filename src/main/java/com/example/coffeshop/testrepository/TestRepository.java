42a408d16feaee76adf84bb594a3842a08141eae
ias <iaszaen@gmail.com>
2022-07-14 14:14:32 +0700

    update

--- src/main/java/com/example/coffeshop/testrepository/TestRepository.java
+++ src/main/java/com/example/coffeshop/testrepository/TestRepository.java
@@ -3,4 +3,6 @@ package com.example.coffeshop.testrepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TestRepository{
+
 }
+
---
+++ src/main/java/com/example/coffeshop/testrepository/testttt.java
@@ -0,0 +1,4 @@
+package com.example.coffeshop.testrepository;
+
+public interface testttt {
+}
